0d2065930bd5fe51e4f90d7e68d6baf322757e0b
Chris Perko <chris@chrisperko.net>
2020-01-07 17:33:49 -0500

    Pages are loaded from json and links are created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Pages } from "../assets/pages/pages.json";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'StaticAngularBlog';
+
+  pageArray = [];
+
+  ngOnInit() {
+    this.pageArray = Pages;
+  }
+
+  getPageLink(title) {
+    return `/page/${title}`;
+  }
 }
