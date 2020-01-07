5c336f8e0d9a54243ccc3201f6ea4cfc6c253ec1
Chris Perko <chris@chrisperko.net>
2020-01-07 17:03:28 -0500

    Added custom landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { MarkdownModule } from 'ngx-markdown';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { PageComponent } from './page/page.component';
+import { HomeComponent } from './custom-pages/home/home.component';
 
 
 const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'about', component: PageComponent },
   { path: 'post/:title', component: PostComponent }
 ];
@@ -21,6 +23,7 @@ const appRoutes: Routes = [
     AppComponent,
     PostComponent,
     PageComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
