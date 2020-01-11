f47c1f0e406c9c1c2429b1fda8f5205387dde493
Chris Perko <chris@chrisperko.net>
2020-01-11 15:52:21 -0500

    Updated script for posts

--- scripts/generatePageIndex.js
+++ scripts/generatePageIndex.js
@@ -2,12 +2,16 @@ const fs = require('fs');
 const path = require('path');
 const metaParser = require('markdown-yaml-metadata-parser');
 const pathToPages = path.join(__dirname, '../src/assets/pages');
+const pathToPosts = path.join(__dirname, '../src/assets/posts');
 const pathToCompiledPages = path.join(__dirname, '../src/_assets/pages');
+const pathToCompiledPosts = path.join(__dirname, '../src/_assets/posts');
 const ignoredFiles = [
   'gitignore',
-  'pages.json'
+  'pages.json',
+  'posts.json'
 ];
 
+// create compiled pages directory
 fs.mkdirSync(pathToCompiledPages, {
   recursive: true
 }, error => {
@@ -16,8 +20,20 @@ fs.mkdirSync(pathToCompiledPages, {
   } else {
     console.log("Directory created successfully!");
   }
-})
+});
 
+// create compiled posts directory
+fs.mkdirSync(pathToCompiledPosts, {
+  recursive: true
+}, error => {
+  if (error) {
+    console.error("An error occurred creating the directory", error);
+  } else {
+    console.log("Directory created successfully!");
+  }
+});
+
+// compile the pages
 fs.readdir(pathToPages, (err, files) => {
   if (err) console.error("Error ready pages", err);
 
@@ -48,6 +64,41 @@ fs.readdir(pathToPages, (err, files) => {
   }
 
   fs.writeFile(`${pathToCompiledPages}/pages.json`, JSON.stringify(data), 'utf8', err => {
+    if (err) console.log(err);
+  });
+});
+
+
+// compile the posts
+fs.readdir(pathToPosts, (err, files) => {
+  if (err) console.error("Error ready posts", err);
+
+  let fileArray = [];
+
+  files.filter(file => !file.includes(ignoredFiles))
+    .forEach(file => {
+      const contents = fs.readFileSync(path.join(__dirname, `../src/assets/posts/${file}`), 'utf8');
+      const parsed = metaParser(contents);
+
+      fs.writeFile(`${pathToCompiledPosts}/${file}`, parsed.content, 'utf8', err => {
+        if (err) {
+          console.error("Error writing file contents", error);
+        }
+      });
+
+      fileArray.push({
+        filename: file,
+        link: file.replace('.md', ''),
+        title: parsed.metadata.title,
+        date: parsed.metadata.date
+      });
+    });
+
+  const data = {
+    Posts: fileArray
+  }
+
+  fs.writeFile(`${pathToCompiledPosts}/posts.json`, JSON.stringify(data), 'utf8', err => {
     if (err) console.log(err);
   });
 });
--- src/assets/posts/2020-01-07-another-post.md
+++ src/assets/posts/2020-01-07-another-post.md
@@ -1,1 +1,9 @@
-# ANother One
+---
+date: 2020-01-07
+title: Another Post
+---
+This is another post
+
+* one
+* two
+* three
