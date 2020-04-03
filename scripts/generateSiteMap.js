017d457287d6290a8738fa26bf1e4cd7497ed42d
Chris Perko <chris@chrisperko.net>
2020-04-03 12:12:34 -0400

    Sitemaps now live in _assets

--- scripts/generateSiteMap.js
+++ scripts/generateSiteMap.js
@@ -10,23 +10,54 @@ const ignoredFiles = [
     'posts.json'
 ];
 
+
+// create the sitemap directory
+const baseSitemapDir = 'src/_assets/sitemaps';
+fs.mkdirSync(baseSitemapDir, {
+    recursive: true
+}, error => {
+    if (error) {
+        console.error("An error occurred creating the directory", error);
+    } else {
+        console.log("Directory created successfully!");
+    }
+});
+
+
 const baseUrl = 'https://www.chrisperko.net/';
+
+const date = new Date().toISOString();
+
+// create the index sitemap
+let indexSitemap = builder.create('root')
+    .ele('sitemapindex', { 'xmlns': 'http://www.sitemaps.org/schemas/sitemap/0.9' })
+    .ele('sitemap')
+    .ele('loc', null, 'https://chrisperko.net/post-sitemap.xml').up()
+    .ele('lastmod', null, date).up().up()
+    .ele('sitemap')
+    .ele('loc', null, 'https://chrisperko.net/page-sitemap.xml').up()
+    .ele('lastmod', null, date)
+    .end({ pretty: true });
+
+fs.writeFile(`${baseSitemapDir}/sitemap.xml`, indexSitemap, 'utf8', err => {
+    if (err) console.log(err);
+});
 
 fs.readFile(`${pathToCompiledPages}/pages.json`, (err, data) => {
     if (err) console.error(err);
 
-    let xml = builder.create('root')
+    let postSitemap = builder.create('root')
         .ele('urlset', { 'xmlns': baseUrl });
 
     const json = JSON.parse(data);
     json.Pages.forEach(page => {
-        xml.ele('url')
+        postSitemap.ele('url')
             .ele('loc', null, `${baseUrl}${page.link}`);
     });
 
-    xml.end({ pretty: true });
+    postSitemap.end({ pretty: true });
 
-    fs.writeFile('sitemap.xml', xml, 'utf8', err => {
+    fs.writeFile(`${baseSitemapDir}/post-sitemap.xml`, postSitemap, 'utf8', err => {
         if (err) console.log(err);
     });
 });
