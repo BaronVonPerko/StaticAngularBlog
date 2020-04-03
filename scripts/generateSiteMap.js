5459758a9e989c4b5a314b6ee30375936c711781
Chris Perko <chris@chrisperko.net>
2020-04-03 11:51:55 -0400

    Sitemap for pages working

--- scripts/generateSiteMap.js
+++ scripts/generateSiteMap.js
@@ -1,20 +1,32 @@
 const fs = require('fs');
 const path = require('path');
-var parser = require('xml2json');
+const builder = require('xmlbuilder');
 
 const pathToCompiledPages = path.join(__dirname, '../src/_assets/pages');
 const pathToCompiledPosts = path.join(__dirname, '../src/_assets/posts');
 const ignoredFiles = [
-  'gitignore',
-  'pages.json',
-  'posts.json'
+    'gitignore',
+    'pages.json',
+    'posts.json'
 ];
 
+const baseUrl = 'https://www.chrisperko.net/';
+
 fs.readFile(`${pathToCompiledPages}/pages.json`, (err, data) => {
-    if(err) console.error(err);
+    if (err) console.error(err);
+
+    let xml = builder.create('root')
+        .ele('urlset', { 'xmlns': baseUrl });
 
     const json = JSON.parse(data);
     json.Pages.forEach(page => {
-        console.log(page.link);
+        xml.ele('url')
+            .ele('loc', null, `${baseUrl}${page.link}`);
+    });
+
+    xml.end({ pretty: true });
+
+    fs.writeFile('sitemap.xml', xml, 'utf8', err => {
+        if (err) console.log(err);
     });
 });
