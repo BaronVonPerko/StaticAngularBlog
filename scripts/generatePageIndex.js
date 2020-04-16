d7deb8c9602a6457b5dba8762fa559351ee6a239
Chris Perko <chris@chrisperko.net>
2020-04-16 17:54:34 -0400

    Updated script to index portfolio

--- scripts/generatePageIndex.js
+++ scripts/generatePageIndex.js
@@ -3,8 +3,10 @@ const path = require('path');
 const metaParser = require('markdown-yaml-metadata-parser');
 const pathToPages = path.join(__dirname, '../src/assets/pages');
 const pathToPosts = path.join(__dirname, '../src/assets/posts');
+const pathToPortfolio = path.join(__dirname, '../src/assets/portfolio');
 const pathToCompiledPages = path.join(__dirname, '../src/_assets/pages');
 const pathToCompiledPosts = path.join(__dirname, '../src/_assets/posts');
+const pathToCompiledPortfolio = path.join(__dirname, '../src/_assets/portfolio');
 const ignoredFiles = [
   'gitignore',
   'pages.json',
@@ -16,9 +18,7 @@ fs.mkdirSync(pathToCompiledPages, {
   recursive: true
 }, error => {
   if (error) {
-    console.error("An error occurred creating the directory", error);
-  } else {
-    console.log("Directory created successfully!");
+    console.error("An error occurred creating the pages directory", error);
   }
 });
 
@@ -27,15 +27,22 @@ fs.mkdirSync(pathToCompiledPosts, {
   recursive: true
 }, error => {
   if (error) {
-    console.error("An error occurred creating the directory", error);
-  } else {
-    console.log("Directory created successfully!");
+    console.error("An error occurred creating the posts directory", error);
   }
 });
 
+// create compiled portfolio directory
+fs.mkdirSync(pathToCompiledPortfolio, {
+  recursive: true
+}, error => {
+  if (error) {
+    console.error("An error occurred creating the portfolio directory", error);
+  }
+})
+
 // compile the pages
 fs.readdir(pathToPages, (err, files) => {
-  if (err) console.error("Error ready pages", err);
+  if (err) console.error("Error reading pages", err);
 
   let fileArray = [];
 
@@ -72,7 +79,7 @@ fs.readdir(pathToPages, (err, files) => {
 
 // compile the posts
 fs.readdir(pathToPosts, (err, files) => {
-  if (err) console.error("Error ready posts", err);
+  if (err) console.error("Error reading posts", err);
 
   let fileArray = [];
 
@@ -103,6 +110,36 @@ fs.readdir(pathToPosts, (err, files) => {
   }
 
   fs.writeFile(`${pathToCompiledPosts}/posts.json`, JSON.stringify(data), 'utf8', err => {
+    if (err) console.log(err);
+  });
+});
+
+
+// compile the portfolio
+fs.readdir(pathToPortfolio, (err, files) => {
+  if (err) console.error("Error reading portfolio", err);
+
+  let fileArray = [];
+
+  files.filter(file => !file.includes(ignoredFiles))
+    .forEach(file => {
+      const contents = fs.readFileSync(path.join(__dirname, `../src/assets/portfolio/${file}`), 'utf8');
+      const parsed = metaParser(contents);
+
+      fileArray.push({
+        title: parsed.metadata.title,
+        image: parsed.metadata.image,
+        type: parsed.metadata.type,
+        description: parsed.metadata.description,
+        url: parsed.metadata.url
+      });
+    });
+
+  const data = {
+    Posts: fileArray
+  }
+
+  fs.writeFile(`${pathToCompiledPortfolio}/portfolio.json`, JSON.stringify(data), 'utf8', err => {
     if (err) console.log(err);
   });
 });
