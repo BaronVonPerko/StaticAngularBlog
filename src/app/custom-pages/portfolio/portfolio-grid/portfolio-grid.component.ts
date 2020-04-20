7f139ffae1c006ea2139af06665b93e4f29b6418
Chris Perko <chris@chrisperko.net>
2020-04-20 17:30:06 -0400

    2 columns on smaller screens

--- src/app/custom-pages/portfolio/portfolio-grid/portfolio-grid.component.ts
+++ src/app/custom-pages/portfolio/portfolio-grid/portfolio-grid.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-portfolio-grid',
   template: `
-  <div class="mt-12 grid gap-5 max-w-lg mx-auto lg:grid-cols-3 lg:max-w-none">
+  <div class="mt-12 grid gap-5 max-w-lg mx-auto lg:grid-cols-2 xl:grid-cols-3 lg:max-w-none">
     <ng-content></ng-content>
   </div>
   `
