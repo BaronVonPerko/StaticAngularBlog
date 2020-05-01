336560947a2f63ddd2185a3c60fbfd4f6743f494
Chris Perko <chris@chrisperko.net>
2020-05-01 15:58:34 -0400

    Completed projects on landing page displays number from portfolios

--- src/app/custom-pages/home/home.component.ts
+++ src/app/custom-pages/home/home.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/services/portfolio.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html'
 })
 export class HomeComponent implements OnInit {
+  portfolioCount: number;
 
-  constructor() { }
+  constructor(private portfolioService: PortfolioService) { }
 
   ngOnInit() {
+    this.portfolioService.getPortfolios()
+      .subscribe(portfolios => this.portfolioCount = portfolios.length);
   }
 
 }
