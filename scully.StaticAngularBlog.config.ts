aa7d9e19d4a5f1f2e8091695b0669cf41b18e38f
Chris Perko <chris@chrisperko.net>
2020-08-05 17:13:46 -0400

    Installed scully

---
+++ scully.StaticAngularBlog.config.ts
@@ -0,0 +1,8 @@
+import { ScullyConfig } from '@scullyio/scully';
+export const config: ScullyConfig = {
+  projectRoot: "./src",
+  projectName: "StaticAngularBlog",
+  outDir: './dist/static',
+  routes: {
+  }
+};
