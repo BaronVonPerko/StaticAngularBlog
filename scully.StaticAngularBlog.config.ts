9bd5f254147670522aa6160ee0c5ea17b21bcc21
Chris Perko <chris@chrisperko.net>
2020-08-10 16:50:48 -0400

    Posts now being rendered in scully

--- scully.StaticAngularBlog.config.ts
+++ scully.StaticAngularBlog.config.ts
@@ -1,8 +1,28 @@
-import { ScullyConfig } from '@scullyio/scully';
+import {HandledRoute, registerPlugin, RouteTypes, ScullyConfig} from '@scullyio/scully';
+
+import {Posts} from './src/_assets/posts/posts.json';
+
+function blogPostPlugin(route: string, config = {}): Promise<HandledRoute[]> {
+  console.log(Posts);
+
+  const routes = [];
+
+  Posts.forEach(post => {
+    routes.push({route: `/blog/post/${post.link}`, type: RouteTypes.json});
+  })
+
+  return Promise.resolve(routes);
+}
+
+registerPlugin('router', 'blogPosts', blogPostPlugin);
+
 export const config: ScullyConfig = {
   projectRoot: "./src",
   projectName: "StaticAngularBlog",
   outDir: './dist/static',
   routes: {
-  }
+    '/blog/post/:title': {
+      type: 'blogPosts',
+    }
+  },
 };
