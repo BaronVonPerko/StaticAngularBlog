142e1563957ceecd99189fb22c4f9e1d8494d583
Chris Perko <chris@chrisperko.net>
2022-04-17 15:53:36 -0400

    Refactored search results to not use subscribe, should also fix#3

--- src/app/components/search-results/search-results.component.ts
+++ src/app/components/search-results/search-results.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { SearchService } from 'src/app/services/search.service';
 import Post from 'src/app/models/post';
+import { mergeMap, Observable, toArray } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-search-results',
@@ -10,22 +12,22 @@ export class SearchResultsComponent implements OnInit, OnChanges {
 
   @Input() searchString: string;
 
-  results: Post[] = [];
+  results$: Observable<Post[]>;
 
   constructor(private searchService: SearchService) {
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    this.results = [];
-    this.searchService.search(changes.searchString.currentValue)
-      .subscribe(post => this.results.push(post));
+    this.results$ = this.searchService.search(changes.searchString.currentValue).pipe(
+      mergeMap(r => r),
+      map(result => {
+        return {...result, link: `/blog/post/${result.link}`};
+      }),
+      toArray()
+    );
   }
 
   ngOnInit(): void {
-  }
-
-  getLink(post: Post): string {
-    return `/blog/post/${post.link}`;
   }
 
 }
