bddc3e9398caaa3a2a315ce6fae20ada5907670c
Chris Perko <chris@chrisperko.net>
2022-04-17 12:51:36 -0400

    Refactored codetips service

--- src/app/services/codetips.service.ts
+++ src/app/services/codetips.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import {from, Observable, of} from 'rxjs';
 import CodeTip from '../models/codetip';
 import { default as CodeTips } from '../../_assets/codetips/codetips.json';
+import {filter, mergeMap, take} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -12,20 +13,14 @@ export class CodetipsService {
   }
 
   getCodeTips(): Observable<CodeTip[]> {
-    return new Observable(subscriber => {
-      const codeTips = CodeTips.CodeTips.sort((a: CodeTip, b: CodeTip) => {
-        const aTime = new Date(a.date).getTime();
-        const bTime = new Date(b.date).getTime();
-        return bTime - aTime;
-      });
-
-      subscriber.next(codeTips);
-    });
+    return of(CodeTips.CodeTips);
   }
 
   getCodeTipDetails(link: string): Observable<CodeTip> {
-    return new Observable(subscriber => {
-      subscriber.next(CodeTips.CodeTips.filter(tip => tip.link === link)[0]);
-    });
+    return this.getCodeTips().pipe(
+      mergeMap(tip => tip),
+      filter(tip => tip.link === link),
+      take(1)
+    );
   }
 }
