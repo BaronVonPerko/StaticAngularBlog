6a955b5fae5281cede2a2d63ad918db5acd0f1b1
Chris Perko <chris@chrisperko.net>
2022-04-17 16:24:20 -0400

    Refactored code tip archive page to not use subscriptions

--- src/app/custom-pages/code-tips-archive/code-tips-archive.component.ts
+++ src/app/custom-pages/code-tips-archive/code-tips-archive.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CodetipsService } from 'src/app/services/codetips.service';
 import CodeTip from 'src/app/models/codetip';
 import { PageHeadService } from '../../services/page-head.service';
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-code-tips-archive',
@@ -10,19 +11,14 @@ import { PageHeadService } from '../../services/page-head.service';
 })
 export class CodeTipsArchiveComponent implements OnInit {
 
-  codeTips: CodeTip[];
+  codeTips$: Observable<CodeTip[]>;
 
   constructor(private codeTipsService: CodetipsService, private pageHeadService: PageHeadService) {
   }
 
   ngOnInit(): void {
-    this.codeTipsService.getCodeTips().subscribe(tips => this.codeTips = tips);
-
+    this.codeTips$ = this.codeTipsService.getCodeTips();
     this.pageHeadService.setTitle('Code Tips');
-  }
-
-  getImage(tip: CodeTip) {
-    return `/assets/images/${tip.image}`;
   }
 
 }
