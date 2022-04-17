b597ed7086987e9b461d723e545f1c0c84b9195e
Chris Perko <chris@chrisperko.net>
2022-04-17 15:31:10 -0400

    Finished refactoring portfolios to not use subscribe

--- src/app/custom-pages/portfolio/portfolio.component.ts
+++ src/app/custom-pages/portfolio/portfolio.component.ts
@@ -1,40 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
 import Portfolio from 'src/app/models/portfolio';
-import { PortfolioService } from 'src/app/services/portfolio.service';
-import { PageHeadService } from '../../services/page-head.service';
-import {flatMap, mergeMap, Observable, Subject, tap, toArray} from "rxjs";
-import {filter, map} from "rxjs/operators";
+import {PortfolioService} from 'src/app/services/portfolio.service';
+import {PageHeadService} from '../../services/page-head.service';
+import {flatMap, mergeMap, Observable, BehaviorSubject, Subject, tap, toArray} from "rxjs";
+import {filter, map, takeUntil} from "rxjs/operators";
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
 })
-export class PortfolioComponent implements OnInit {
+export class PortfolioComponent implements OnInit, OnDestroy {
   portfolios$: Observable<Portfolio[]>;
   uniqueTypes$: Observable<string[]>;
+  selectedType$ = new BehaviorSubject<string>('All');
+  onDestroy$ = new Subject();
   selectedItem: Portfolio;
-  selectedType$: Subject<string>;
 
   constructor(private portfolioService: PortfolioService, private pageHeadService: PageHeadService) {
   }
 
   ngOnInit(): void {
-    this.portfolios$ = this.portfolioService.getPortfolios().pipe(
-      tap(p => console.log(p)),
-      tap(p => console.log(this.selectedType$)),
-      map(portfolios => portfolios.filter(portfolio => portfolio.type === this.selectedType$))
-    );
+    this.selectedType$.pipe(
+      takeUntil(this.onDestroy$)
+    ).subscribe(selectedType => {
+      this.portfolios$ = this.portfolioService.getPortfolios().pipe(
+        map(portfolios => {
+            if (selectedType !== 'All') {
+              return portfolios.filter(portfolio => portfolio.type === selectedType)
+            }
+            return portfolios;
+          }
+        )
+      );
+    })
+
     this.uniqueTypes$ = this.portfolioService.getUniqueTypes();
     this.pageHeadService.setTitle('Portfolio');
   }
 
-  onTypeClicked(selectedType) {
-    this.selectedType$.next(selectedType);
+  ngOnDestroy() {
+    this.onDestroy$.next(true);
   }
 
-  onClickAll() {
-    // this.loadAllPortfolios();
-    // this.selectedType = null;
+  onTypeClicked(selectedType) {
+    this.selectedType$.next(selectedType);
   }
 
 }
